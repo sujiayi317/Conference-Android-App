55acbde1f92c94e64bab1514c762968d252a0897
Jiayi Su <sjy.su@mail.utoronto.ca>
2020-11-05 22:58:59 -0500

    Add OrganizerController class, unfinished version

---
+++ phase1/OrganizerController.java
@@ -0,0 +1,33 @@
+//import java.io.Serializable;
+
+/**
+ * The OrganizerController class, this is used as a Controller for what organizers can do.
+ *
+ */
+public class OrganizerController{
+
+    /**
+     * Create an event
+     */
+    public void createEvent(Event event) {
+        // TODO
+    }
+
+    /**
+     * Remove an event from the room
+     * @param event Event being removed
+     */
+    public void removeEvent(Event event) {
+        // TODO
+    }
+
+    /**
+     * Checks if the time slot is taken
+     * @param time Integer time (hour) being checked
+     * @return true if time slot is taken
+     */
+    public boolean isTimeslotTaken(int time) {
+        // TODO
+        return false;
+    }
+}
