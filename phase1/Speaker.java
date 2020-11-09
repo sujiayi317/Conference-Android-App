07968c18d9f7ad71ee54f66412ed8bdfe5825e2e
hardygu <hardy.gu@mail.utoronto.ca>
2020-11-09 09:40:07 -0500

    test

--- phase1/Speaker.java
+++ phase1/Speaker.java
@@ -9,7 +9,6 @@ public class Speaker extends User implements Serializable {
 
     public Speaker(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
-
     }
-
+    //Hardy tried to push
 }
