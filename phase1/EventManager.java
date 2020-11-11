ebdb1fe9ae30749103c08a0ae52ba71972487322
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-11-10 23:50:13 -0500

    Version_7.0 for EventManager

--- phase1/EventManager.java
+++ phase1/EventManager.java
@@ -6,7 +6,7 @@ import java.util.List;
  * The EventManager class, this is the use case class to manage all events.
  *
  */
-public class EventManager implements Serializable{
+public class EventManager implements Serializable {
 
     private List<Event> events;
 
@@ -14,7 +14,7 @@ public class EventManager implements Serializable{
     /**
      * Creates an empty event manager
      */
-    public EventManager(){
+    public EventManager() {
         events = new ArrayList<>();
 
     }
@@ -25,7 +25,8 @@ public class EventManager implements Serializable{
      */
     public boolean createEvent(String title, String roomID, Speaker speaker, int startTime) {
         for (Event event : this.events) {
-            return ! ((event.getSpeakers().contains(speaker.getUserName())|| roomID.equals(event.getRoomID()) && event.getStartTime() == startTime));}
+            return !((event.getSpeakers().contains(speaker.getUserName()) || roomID.equals(event.getRoomID()) && event.getStartTime() == startTime));
+        }
         Event newEvent = new Event(title, roomID, speaker, startTime);
         events.add(newEvent);
         return true;
@@ -38,18 +39,27 @@ public class EventManager implements Serializable{
      * So the controller method calls the EventManager method with two String parameters: userName and eventName.
      * The the EventManager, looks through the list of Event objects and finds the one with the correct name and
      * calls addUser(userName).
-     *
+     * <p>
      * Note: This would add a String with the username to a list of Strings inside the Event, not the User object
      * itself. If you want to add the Even to the User, the Controller would send only the String eventName to the
      * UserManager, to store the Event's name in a list of Strings inside the User object."
      */
     public boolean addAttendeeToEvent(String userID, String eventID) {
-        for (Event event: events) {
-            if (event.getEventID().equals(eventID)) {
-                return event.addAttendee(userID, events);
-            }
-        }
-        return false;
+            Event event = getEventFromID(eventID);
+            return event.addAttendee(userID, events);}
+
+
+    public ArrayList<String> getAttendeesToEvent(String eventID) {
+        Event event = getEventFromID(eventID);
+        return event.getAttendees();
     }
 
+    public Event getEventFromID(String eventID) {
+        for (Event event : events) {
+            if (event.getEventID().equals(eventID)) {
+                return event;
+            }
+        }
+        return null;
+    }
 }
