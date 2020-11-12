da74604ecf6998908f4f0a7f7e1546f6d0605ae4
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-11-12 14:12:49 -0500

    Version_4.0 for EventController

--- phase1/Presenter/ViewEventInfo.java
+++ phase1/Presenter/ViewEventInfo.java
@@ -1,7 +1,24 @@
 package Presenter;
 
+import java.util.ArrayList;
+
 public class ViewEventInfo {
-    public void getEventInfo(String eventID){
+    private ArrayList<String> infoPattern;
+    public void ViewEventInfo(){
+        this.infoPattern.add("Title");
+        this.infoPattern.add("Time");
+        this.infoPattern.add("Speakers");
+        this.infoPattern.add("Current_Capacity");
+
+    }
+    public void getEventInfo(ArrayList<String> eventInfo){
+        StringBuilder returnString = new StringBuilder("There are all existing events you may attend:");
+        for (int i =0 ; i < eventInfo.size()-2;i++){
+            returnString.append(this.infoPattern.get(i)).append(":").append(eventInfo.get(i));
+        }
+        returnString.append(this.infoPattern.get(3)).append(":").append(eventInfo.get(3)).append("/").append(eventInfo.get(4));
+        System.out.println(returnString);
+    }
 
     }
 }
