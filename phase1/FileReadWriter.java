5ec12da905671f95d64b55f693a820ed873473fa
Yueming Ma <yueming.ma@mail.utoronto.ca>
2020-11-11 21:24:34 -0500

    Added Login.java and Added new method readFromUserFile to FileReadWriter.java

--- phase1/FileReadWriter.java
+++ phase1/FileReadWriter.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.ArrayList;
+
 class FileReadWriter {
 
     public EventManager readFromEventFile(String path) throws ClassNotFoundException {
@@ -48,6 +51,37 @@ class FileReadWriter {
             logger.log(Level.SEVERE, "Cannot read from input file, returning" +
                     "a new RoomManager.", ex);
             return new RoomManager();
+        }
+    }
+
+    public ArrayList<User> readFromUserFile(String path) throws ClassNotFoundException {
+        try {
+            InputStream file = new FileInputStream(path); // String path should be "fileName.ser"
+            InputStream buffer = new BufferedInputStream(file);
+
+            ArrayList<User> objList = new ArrayList<>();
+            boolean load = true;
+            while (load)
+            {
+                try (ObjectInput input = new ObjectInputStream(buffer))
+                {
+                    User sm = (User) input.readObject();
+                    if (sm != null) {
+                        objList.add(sm);
+                    } else {
+                        load = false;
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            }
+            input.close(); //can't close input
+            return objList;
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Cannot read from input file, returning" +
+                    "a new User.", ex);
+            return new ArrayList<>();
         }
     }
 }
