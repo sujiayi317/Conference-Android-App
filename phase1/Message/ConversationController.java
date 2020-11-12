85aa2ee3946b0f9be4fc9a576e8e1c1a4781ef94
hardygu <hardy.gu@mail.utoronto.ca>
2020-11-12 15:52:26 -0500

    many changes

---
+++ phase1/Message/ConversationController.java
@@ -0,0 +1,11 @@
+package Message;
+
+public class ConversationController {
+    private ConversationManager conversationManager;
+
+    public void ConversationController(){
+        this.conversationManager = new ConversationManager();
+    }
+
+
+}
