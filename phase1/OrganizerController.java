f210f95d5adec40d9918fd64b500454a8b64e890
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-12 17:54:00 -0500

    Demo v0.1

--- phase1/OrganizerController.java
+++ phase1/OrganizerController.java
@@ -1,51 +1,51 @@
-//import java.io.Serializable;
-
-/**
- * The OrganizerController class, this is used as a Controller for what organizers can do.
- *
- */
-
-import entities.Event;
-import entities.Speaker;
-import use_cases.EventManager;
-import use_cases.RoomManager;
-import use_cases.SpeakerManager;
-
-/**
- * The OrganizerController class, this is the Controller class for an entities.Organizer.
- *
- */
-public class OrganizerController{
-
-    FileReadWriter fileReadWriter = new FileReadWriter();
-
-    private EventManager em = fileReadWriter.readFromEventFile("event.ser");
-    private SpeakerManager sm = fileReadWriter.readFromSpeakerFile("speaker.ser");
-    private RoomManager rm = fileReadWriter.readFromRoomFile("room.ser");
-
-
-    /**
-     * Create an event
-     */
-    public Event createEvent(String title, String roomID, String speakerID, int startTime) {
-        em.createEvent(title, roomID, speakerID, startTime);
-    }
-
-    /**
-     * Remove an event from the room
-     * @param event entities.Event being removed
-     */
-    public void removeEvent(Event event) {
-        // TODO
-    }
-
-    /**
-     * Checks if the time slot is taken
-     * @param time Integer time (hour) being checked
-     * @return true if time slot is taken
-     */
-    public boolean isTimeslotTaken(int time) {
-        // TODO
-        return false;
-    }
-}
+////import java.io.Serializable;
+//
+///**
+// * The OrganizerController class, this is used as a Controller for what organizers can do.
+// *
+// */
+//
+//import entities.Event;
+//import entities.Speaker;
+//import use_cases.EventManager;
+//import use_cases.RoomManager;
+//import use_cases.SpeakerManager;
+//
+///**
+// * The OrganizerController class, this is the Controller class for an entities.Organizer.
+// *
+// */
+//public class OrganizerController{
+//
+//    FileReadWriter fileReadWriter = new FileReadWriter();
+//
+//    private EventManager em = fileReadWriter.readFromEventFile("event.ser");
+//    private SpeakerManager sm = fileReadWriter.readFromSpeakerFile("speaker.ser");
+//    private RoomManager rm = fileReadWriter.readFromRoomFile("room.ser");
+//
+//
+//    /**
+//     * Create an event
+//     */
+//    public Event createEvent(String title, String roomID, String speakerID, int startTime) {
+//        em.createEvent(title, roomID, speakerID, startTime);
+//    }
+//
+//    /**
+//     * Remove an event from the room
+//     * @param event entities.Event being removed
+//     */
+//    public void removeEvent(Event event) {
+//        // TODO
+//    }
+//
+//    /**
+//     * Checks if the time slot is taken
+//     * @param time Integer time (hour) being checked
+//     * @return true if time slot is taken
+//     */
+//    public boolean isTimeslotTaken(int time) {
+//        // TODO
+//        return false;
+//    }
+//}
--- phase1/controllers/Login.java
+++ phase1/controllers/Login.java
@@ -14,7 +14,7 @@ public class Login{
 
     public void run(AttendeeManager attendeeManager, OrganizerManager organizerManager, SpeakerManager speakerManager) {
         while (true) {
-            output.printPrompt("Welcome to main page of conference sign up centre! Please enter a number");
+            output.printPrompt("Welcome to main page of conference sign up centre! Please enter a number\n");
             Integer CurrentAction = input.getInputInt("1. Sign in \n2. Create an account");
             if (CurrentAction == 1) {
                 if (signIn(attendeeManager, organizerManager, speakerManager)){
@@ -30,8 +30,8 @@ public class Login{
 
     public boolean signIn(AttendeeManager attendeeManager, OrganizerManager organizerManager, SpeakerManager speakerManager){
         while (true) {
-            String account = input.getInputString("Please enter your email:");
-            String password = input.getInputString("Please enter your password:");
+            String account = input.getInputString("Please enter your email:\n");
+            String password = input.getInputString("Please enter your password:\n");
 //            if (attendeeManager.validLogIn(account, password).equals("NULL") &&
 //                    speakerManager.validLogIn(account, password).equals("NULL") &&
 //                    organizerManager.validLogIn(account, password).equals("NULL")){
@@ -53,7 +53,7 @@ public class Login{
 
     public boolean createAccount(AttendeeManager attendeeManager, OrganizerManager organizerManager, SpeakerManager speakerManager){
         while (true) {
-            output.printPrompt("Select what type of account you want to create, or enter 'cancel' to exit.");
+            output.printPrompt("Select what type of account you want to create, or enter 'cancel' to exit.\n");
             String CurrentAction = input.getInputString("1. Attendee account \n2. Organizer account");
             if (CurrentAction.equals("1")) {
                 return createAccount.CreateNewAccount(attendeeManager, organizerManager, speakerManager, "ATTENDEE");
@@ -62,7 +62,7 @@ public class Login{
             } else if(CurrentAction.equals("cancel")){
                 return false;
             } else {
-                output.printPrompt("Invalid action, please try again.");
+                output.printPrompt("Invalid action, please try again.\n");
             }
         }
     }
