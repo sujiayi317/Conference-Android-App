8c6d3ff50d76edd76a4e4d3a0045b2d8db2b9a47
hardygu <hardy.gu@mail.utoronto.ca>
2020-11-11 22:31:05 -0500

    entity conversation

---
+++ phase1/Conversation.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util;
+
+public class Conversation {
+    private String userID1;
+    private String userID2;
+    private ArrayList<Pair> userID;
+    private ArrayList<String> speakers;
+    private int startTime;
+    private int duration = 1;
+}
