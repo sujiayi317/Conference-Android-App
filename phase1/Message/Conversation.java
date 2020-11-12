73b76e828bae406efeb7019c3ad611f2771ece4c
hardygu <hardy.gu@mail.utoronto.ca>
2020-11-11 23:30:48 -0500

    entity 11.10

--- phase1/Message/Conversation.java
+++ phase1/Message/Conversation.java
@@ -2,12 +2,13 @@ package Message;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
-public class Conversation {
-    private String userID1;
-    private String userID2;
-    private ArrayList<String []> userID;
+public class Conversation implements Serializable {
+    private String[] userIds = new String[2]; //Store uerIds of the two users
+    private ArrayList<String[]> messages = new ArrayList<String[]>();
 
-    public Conversation();
+    public Conversation(String userId1, String userId2){
+        userIds[0] = userId1;
+        userIds[1] = userId2;
+    }
 }
