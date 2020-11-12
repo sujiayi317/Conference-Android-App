8a7cad9e7bd8b9ca376fc70c330e844307efac5c
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-11-12 17:27:00 -0500

    Version_11.0 for EventController

--- phase1/Presenter/AttendeeMenu.java
+++ phase1/Presenter/AttendeeMenu.java
@@ -1,8 +1,18 @@
 package Presenter;
 
-public class AttendeeMenu {
+import controllers.OutputManager;
+import entities.Attendee;
 
-    public void printAttendeeMenu(){
+public class AttendeeMenu {
+    private static OutputManager output;
+    public AttendeeMenu(){
+        output = new OutputManager();
+    }
+    public void printAttendeeMenu(String userID){
+        StringBuilder returnString = new StringBuilder("Welcome");
+        returnString.append(userID).append("\n");
+        returnString.append("1) viewAllEvents\n 2)view all my events\n 3)View all my fiends\n 4)view all my message");
+        output.printPrompt(returnString);
 
     }
 
