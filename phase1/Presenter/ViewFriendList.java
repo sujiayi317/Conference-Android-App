a8ff957ae055c5a07ba7361003d2998757d239a6
xiaotonghuang <xiaotong.huang@mail.utoronto.ca>
2020-11-13 11:13:46 -0500

    minor change

--- phase1/Presenter/ViewFriendList.java
+++ phase1/Presenter/ViewFriendList.java
@@ -7,7 +7,7 @@ public class ViewFriendList {
     public StringBuilder getFriendList(ArrayList<String> friendList){
         StringBuilder returnList = new StringBuilder("Friends:\n");
         for (int i =0 ; i < friendList.size();i++){
-            returnList.append(i).append(friendList.get(i)).append("\n");
+            returnList.append(i).append("0").append(friendList.get(i)).append("\n");
         }
         return returnList;
     }
