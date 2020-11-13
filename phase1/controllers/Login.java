c1881a53be261c252eab8b744feee56da1fec631
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-13 15:26:14 -0500

    Login in prompt fix3

--- phase1/controllers/Login.java
+++ phase1/controllers/Login.java
@@ -20,8 +20,9 @@ public class Login{
     public void run(AttendeeManager attendeeManager, OrganizerManager organizerManager, SpeakerManager speakerManager,
                     UserManager usermanager) {
         while (true) {
-            output.printPrompt("\n*** Welcome to login page of conference sign up center! ***\n *** Please enter '1' " +
-                    "or '2' to Login or create account ***\n*** or enter 'EXIT' to fully exit program. ***\n");
+            output.printPrompt("\n*** Welcome to login page of conference sign up center! ***\n*** Please enter '1' " +
+                    "or '2' to Login or create account, ***\n***      or enter 'EXIT' to save and exit program." +
+                    "      ***\n");
             String CurrentAction = input.getInputString("1. Sign in \n2. Create an account\n");
             if (CurrentAction.equals("1")) {
                 if (signIn(usermanager)){
