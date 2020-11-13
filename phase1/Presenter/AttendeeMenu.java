d3810b9c5104eea2ab5af06cee8aca142cf735f5
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-11-13 14:24:03 -0500

    nicer presenter design

--- phase1/Presenter/AttendeeMenu.java
+++ phase1/Presenter/AttendeeMenu.java
@@ -10,7 +10,7 @@ public class AttendeeMenu {
         output = new OutputManager();
     }
     public void printAttendeeMenu(String userName){
-        StringBuilder returnString = new StringBuilder("\nWelcome ");
+        StringBuilder returnString = new StringBuilder("\nWelcome Attendee: ");
         returnString.append(userName).append("\n");
         returnString.append(" 1) viewAllEvents\n 2) view all my events\n 3) View all my fiends\n 4) view all my message" +
                 "\n 5) add friend\n 0) quit\n");
