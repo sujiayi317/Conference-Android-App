d0f3bcba3a7570f1e64cfbd6d1b7022318d8cdc4
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-13 15:30:29 -0500

    Sign out prompt fix

--- phase1/Presenter/AttendeeMenu.java
+++ phase1/Presenter/AttendeeMenu.java
@@ -13,7 +13,7 @@ public class AttendeeMenu {
         StringBuilder returnString = new StringBuilder("\nWelcome Attendee: ");
         returnString.append(userName).append("\n");
         returnString.append(" 1) viewAllEvents\n 2) view all my events\n 3) View all my fiends\n 4) view all my message" +
-                "\n 5) add friend\n 0) quit\n");
+                "\n 5) add friend\n 0) sign out\n");
         output.printPrompt(returnString);
 
     }
