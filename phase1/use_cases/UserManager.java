bef4bf3892d86da7848216b749d00550477ad54a
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-13 00:20:59 -0500

    User syntax fix

--- phase1/use_cases/UserManager.java
+++ phase1/use_cases/UserManager.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserManager {
-    public static  List<User> users;
+    public static List<User> users;
 
     public UserManager(){
         users = new ArrayList<>();
