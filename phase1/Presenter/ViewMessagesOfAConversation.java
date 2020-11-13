577fb84fcc94f6f88dc466bccee20f3d28e600d9
hardygu <hardy.gu@mail.utoronto.ca>
2020-11-12 21:44:09 -0500

    presenter added

--- phase1/Presenter/ViewMessagesOfAConversation.java
+++ phase1/Presenter/ViewMessagesOfAConversation.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 public class ViewMessagesOfAConversation {
     public ViewMessagesOfAConversation(){}
 
-    public String printMessages(ArrayList<String> listOfStrings){
+    public StringBuilder printMessages(ArrayList<String> listOfStrings){
         StringBuilder returnString = new StringBuilder();
-
+        for(String str: listOfStrings){
+            returnString.append(str);
+            returnString.append("\n");
+        }
+        return returnString;
     }
 }
