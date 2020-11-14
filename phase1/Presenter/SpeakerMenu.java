38264f21791cb08334b3a354f2df748346869747
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-11-14 02:34:18 -0500

    SpeakerMenu

--- phase1/Presenter/SpeakerMenu.java
+++ phase1/Presenter/SpeakerMenu.java
@@ -8,7 +8,7 @@ public class SpeakerMenu {
 
     public SpeakerMenu() { output = new OutputManager(); }
     public void printSpeakerMenu(String userID){
-        StringBuilder returnString = new StringBuilder("Welcome ");
+        StringBuilder returnString = new StringBuilder("Welcome Speaker: ");
         returnString.append(userID).append("\n");
         returnString.append(" 1) view all my events\n 2) view my announcements\n 0) sign out\n");
         output.printPrompt(returnString);
