5baa4128a858440f9f991af0d10de0dd3c2fe2e6
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-13 19:05:41 -0500

    get room number input non digit crash fix

--- phase1/Message/SaveConversation.java
+++ phase1/Message/SaveConversation.java
@@ -1,18 +1,18 @@
-package Message;
-
-//package serialization;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-
-public class SaveConversation {
-    public static final String FILE_PATH = "file.ser";
-
-    public void save(Conversation conversation){
-        File file;
-        Read
-    }
-}
+//package Message;
+//
+////package serialization;
+//
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.ObjectOutputStream;
+//import java.util.ArrayList;
+//
+//public class SaveConversation {
+//    public static final String FILE_PATH = "file.ser";
+//
+//    public void save(Conversation conversation){
+//        File file;
+//        Read
+//    }
+//}
