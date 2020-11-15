2f3f78cdc1d8d055722e5b9c5ab43fb273f111a1
Jiayi Su <sjy.su@mail.utoronto.ca>
2020-11-15 15:53:31 -0500

    Add some Javadoc to controllers.See*.java

--- phase1/controllers/SeeALLMyEvents.java
+++ phase1/controllers/SeeALLMyEvents.java
@@ -2,13 +2,27 @@ package controllers;
 
 import presenters.ViewAllAttendeeEvents;
 
+/**
+ * This is the class for viewing all my events
+ */
 public class SeeALLMyEvents {
     private static OutputManager output;
-    public SeeALLMyEvents(){
+
+    /**
+     * SeeALLMyEvents ...
+     */
+    public SeeALLMyEvents() {
         output = new OutputManager();
     }
+
+    /**
+     *
+     * @param viewAllAttendeeEvents viewAllAttendeeEvents
+     * @param eventsController eventsController
+     * @param userID userID
+     */
     public void getToSeeAllMyEvents(ViewAllAttendeeEvents viewAllAttendeeEvents,
-                                    EventsController eventsController, String userID){
+                                    EventsController eventsController, String userID) {
         output.printPrompt(viewAllAttendeeEvents.printAllAttendeeEvents(eventsController.getALLAttendeeEvents(userID), eventsController));
     }
 }
--- phase1/controllers/SeeAllMessage.java
+++ phase1/controllers/SeeAllMessage.java
@@ -6,41 +6,58 @@ import use_cases.UserManager;
 
 import java.util.ArrayList;
 
+/**
+ * This is the class for viewing all messages
+ */
 public class SeeAllMessage {
     private static OutputManager output;
     private static InputManager input;
 
-    public SeeAllMessage(){
+    public SeeAllMessage() {
         output = new OutputManager();
         input = new InputManager();
     }
 
+    /**
+     * See All Message
+     *
+     * @param userID userID
+     * @param userManager userManager
+     * @param conversationController conversationController
+     * @param viewMessageList viewMessageList
+     */
     public void getToSeeAllMessage(String userID, UserManager userManager, ConversationController conversationController,
-                              ViewMessageList viewMessageList){
+                                   ViewMessageList viewMessageList) {
         ArrayList<String[]> messageList = conversationController.getUserAllConversation(userID);
 
         ArrayList<String[]> userNameWithLastMessage = new ArrayList<>();
-        for (String[] s: messageList){
-            String[] msg = {userManager.getUserName(s[2]), s[3], s[0],s[1]};
+        for (String[] s : messageList) {
+            String[] msg = {userManager.getUserName(s[2]), s[3], s[0], s[1]};
             userNameWithLastMessage.add(msg);
         }//有问题
         boolean check4 = false;
-        while (!check4){
+        while (!check4) {
             viewMessageList(userNameWithLastMessage, viewMessageList);
             int chooseConversation = input.getInputInt("Choose a message to start the conversation, or type 0 to quit\n") - 1;
-            if (0 <= chooseConversation && chooseConversation <= messageList.size()-1){
-                if (messageList.get(chooseConversation)[0].equals(userID)){
+            if (0 <= chooseConversation && chooseConversation <= messageList.size() - 1) {
+                if (messageList.get(chooseConversation)[0].equals(userID)) {
                     conversationController.enterConversation(messageList.get(chooseConversation)[1]);
-                }else{
+                } else {
                     conversationController.enterConversation(messageList.get(chooseConversation)[0]);
                 }
                 check4 = true;
-            }else if (chooseConversation == -1){
-                check4 =true;
+            } else if (chooseConversation == -1) {
+                check4 = true;
             }
         }
     }
-    public static void viewMessageList(ArrayList<String[]> conversations, ViewMessageList viewMessageList){
+
+    /**
+     * View the list of all messages
+     * @param conversations conversations
+     * @param viewMessageList viewMessageList
+     */
+    public static void viewMessageList(ArrayList<String[]> conversations, ViewMessageList viewMessageList) {
         output.printPrompt(viewMessageList.getMessageList(conversations));
     }
 }
