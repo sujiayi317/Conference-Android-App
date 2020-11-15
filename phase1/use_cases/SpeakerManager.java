d33c2461357db8f41b5b8c3b7c613030aef60a28
Jiayi Su <sjy.su@mail.utoronto.ca>
2020-11-15 13:02:31 -0500

    Complete OrganizerManager and SpeakerManager Javadoc

--- phase1/use_cases/SpeakerManager.java
+++ phase1/use_cases/SpeakerManager.java
@@ -9,88 +9,76 @@ import java.util.List;
 
 /**
  * The use_cases.SpeakerManager class, this is the use case class to manage the entities.Speaker for this conference.
- *
  */
 public class SpeakerManager extends UserManager implements Serializable {
-    //private List<String> events;
-    private List<Speaker> speakers;
 
-    //private Map<String, entities.Speaker> speakerMap;
-    //private Map<String, List<String>> eventsMap;
+    private List<Speaker> speakers;
 
     /**
      * Creates an use_cases.SpeakerManager with lists of Events for a speaker that is empty
      */
-    public SpeakerManager(){
-//        events = new ArrayList<>();
-//        speakerMap = new HashMap<>();
-//        eventsMap = new HashMap<>();
+    public SpeakerManager() {
         speakers = new ArrayList<>();
     }
 
     /**
-     * Creates a entities.Speaker and adds it to the map and lists
+     * Creates a entities.Speaker and adds it to the lists
+     *
+     * @param userName userName of this speaker
+     * @param email email of this speaker
+     * @param password password of this speaker
      */
     public void createSpeaker(String userName, String email, String password) {
         Speaker speaker = new Speaker(userName, email, password);
-        //speakerMap.put(speaker.getUserName(), speaker);
         speakers.add(speaker);
         UserManager.users.add(speaker);
     }
 
     /**
-     * Load a entities.Speaker from file and adds it to the map and lists
+     * Load a entities.Speaker from file and adds it to the lists
+     *
+     * @param userName userName of this speaker
+     * @param email email of this speaker
+     * @param password password of this speaker
+     * @param ID user ID of this speaker
      */
     public void loadSpeaker(String userName, String email, String password, String ID) {
         Speaker speaker = new Speaker(userName, email, password, ID);
-        //speakerMap.put(speaker.getUserName(), speaker);
         speakers.add(speaker);
         UserManager.users.add(speaker);
     }
 
-    public boolean validNewSpeakerName(String name){
-        for (Speaker speaker : speakers){
-            if (speaker.getUserName().equals(name)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean validNewSpeakerEmail(String email){
-        for (Speaker speaker : speakers){
-            if (speaker.getEmail().equals(email)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-
     /**
      * Check email and password, return its userID (or "NULL")
-     * @param email email
+     *
+     * @param email    email
      * @param password password
-     * @return userID or "NULL"
+     * @return userID of the speaker, or "NULL" if not found
      */
-    public String validLogIn(String email, String password){
-        for (Speaker speaker : speakers){
-            if (speaker.getEmail().equals(email) && speaker.getPassword().equals(password)){
+    public String validLogIn(String email, String password) {
+        for (Speaker speaker : speakers) {
+            if (speaker.getEmail().equals(email) && speaker.getPassword().equals(password)) {
                 return speaker.getUserID();
-            } else if (speaker.getEmail().equals(email)){
+            } else if (speaker.getEmail().equals(email)) {
                 return "NULL";
             }
         }
         return "NULL";
     }
 
-    public ArrayList<String> getAllAvailableSpeaker(String time, EventManager eventManager){
+    /**
+     * Get an ArrayList<String> of all Available Speakers given a starting time
+     * @param time String, start time
+     * @param eventManager an EventManager
+     * @return ArrayList<String> of all Available Speaker
+     */
+    public ArrayList<String> getAllAvailableSpeaker(String time, EventManager eventManager) {
         ArrayList<String> availableSpeaker = new ArrayList<>();
         List<Event> events = eventManager.getAllEvent();
-        for (Speaker speaker : speakers){
+        for (Speaker speaker : speakers) {
             availableSpeaker.add(speaker.getUserName());
-            for (Event event : events){
-                if (event.getSpeakers().equals(speaker.getUserName()) && time.equals(event.getStartTime())){
+            for (Event event : events) {
+                if (event.getSpeakers().equals(speaker.getUserName()) && time.equals(event.getStartTime())) {
                     availableSpeaker.remove(speaker.getUserName());
                 }
             }
@@ -98,23 +86,17 @@ public class SpeakerManager extends UserManager implements Serializable {
         return availableSpeaker;
     }
 
-    public String getIdFromName(String name){
-        for (Speaker speaker : speakers){
-            if (speaker.getUserName().equals(name)){
+    /**
+     * Get ID of a speaker given his user name
+     * @param name name of the speaker
+     * @return user ID of the speaker, or "NULL" if not found
+     */
+    public String getIdFromName(String name) {
+        for (Speaker speaker : speakers) {
+            if (speaker.getUserName().equals(name)) {
                 return speaker.getUserID();
             }
         }
         return "NULL";
     }
-
-//    public void addEventToSpeaker(String event, entities.Speaker speaker) {
-//        events = eventsMap.get(speaker.getUserName());
-//        events.add(event);
-//        eventsMap.put(speaker.getUserName(), events);
-//    }
-
-//    public Map<String, List<String>> getEventsMap() {
-//        return eventsMap;
-//    }
-
 }
