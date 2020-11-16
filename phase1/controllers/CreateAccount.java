d50a5ee5718a9bcdee38c7d3edd17c50e5b6ef00
Deshang Kong <deshang.kong@mail.utoronto.ca>
2020-11-15 21:27:18 -0500

    textUI fix and input no space required

--- phase1/controllers/CreateAccount.java
+++ phase1/controllers/CreateAccount.java
@@ -11,18 +11,23 @@ public class CreateAccount {
 
     /**
      * The method of creating an new account.
-     * @param attendeeManager attendee manager class
+     *
+     * @param attendeeManager  attendee manager class
      * @param organizerManager organizer manager class
-     * @param speakerManager speaker manager class
-     * @param userManager user manager class
-     * @param type the type of account the user wants to create.
+     * @param speakerManager   speaker manager class
+     * @param userManager      user manager class
+     * @param type             the type of account the user wants to create.
      * @return true iff the account is successfully created.
      */
     public boolean CreateNewAccount(AttendeeManager attendeeManager, OrganizerManager organizerManager,
                                     SpeakerManager speakerManager, UserManager userManager, String type) {
         String email = input.getInputString("Please enter the email for new speaker: (ex. 12345@abc.com), or enter 'cancel' at any point to exit account creation\n");
+        if (email.contains(" ")) {
+            return false;
+        }
+        ;
         while (true) {
-            if (email.equals("cancel")){
+            if (email.equals("cancel")) {
                 return false;
             } else if (isValidEmail(email, userManager)) {
                 break;
@@ -32,8 +37,11 @@ public class CreateAccount {
         }
 
         String user = input.getInputString("Please enter a user name for new account: (must have length of at least 2), or enter 'cancel' at any point to exit account creation\n");
+        if (user.contains(" ")){
+            return false;
+        };
         while (true) {
-            if (user.equals("cancel")){
+            if (user.equals("cancel")) {
                 return false;
             } else if (isValidUserName(user, userManager)) {
                 break;
@@ -42,8 +50,11 @@ public class CreateAccount {
             }
         }
         String password = input.getInputString("Please enter a password for " + user + ":\n");
+        if (password.contains(" ")){
+            return false;
+        };
         while (true) {
-            if (password.equals("cancel")){
+            if (password.equals("cancel")) {
                 return false;
             } else if (password.length() >= 8) {
                 break;
@@ -51,9 +62,9 @@ public class CreateAccount {
                 password = input.getInputString("Password must be at least length 8, please try again, or enter 'cancel' at any point to exit account creation\n");
             }
         }
-        if (type.equals("SPEAKER")){
+        if (type.equals("SPEAKER")) {
             speakerManager.createSpeaker(email, user, password);
-        } else if (type.equals("ORGANIZER")){
+        } else if (type.equals("ORGANIZER")) {
             organizerManager.createOrganizer(email, user, password);
         } else {
             attendeeManager.createAttendee(email, user, password);
@@ -63,18 +74,19 @@ public class CreateAccount {
 
     /**
      * The method to check if a given email is in valid form, or already existed in the system.
-     * @param email The given email.
+     *
+     * @param email       The given email.
      * @param userManager user manager class.
      * @return true iff the email is valid.
      */
-    public boolean isValidEmail(String email, UserManager userManager){
-        if (!userManager.validNewEmail(email)){
+    public boolean isValidEmail(String email, UserManager userManager) {
+        if (!userManager.validNewEmail(email)) {
             new OutputManager().printPrompt("Email already exists in the system...\n");
             return false;
         } else if (email.length() >= 6 && email.contains("@") && email.charAt(0) != '@' && email.contains(".") &&
                 email.charAt(email.length() - 1) != '.' && email.length() - email.replace(".", "").length() == 1 &&
                 email.length() - email.replace("@", "").length() == 1 && email.indexOf('@') < email.indexOf('.') &&
-                email.indexOf('@') != email.indexOf('.') - 1 && userManager.validNewEmail(email) ){
+                email.indexOf('@') != email.indexOf('.') - 1 && userManager.validNewEmail(email)) {
             return true;
         } else {
             new OutputManager().printPrompt("Invalid Email format...\n");
@@ -84,11 +96,12 @@ public class CreateAccount {
 
     /**
      * The check if the user name is valid.
-     * @param user the user name.
+     *
+     * @param user        the user name.
      * @param userManager user manager class.
      * @return true iff the name is valid.
      */
-    public boolean isValidUserName(String user, UserManager userManager){
+    public boolean isValidUserName(String user, UserManager userManager) {
         return user.length() >= 2 && userManager.validNewName(user);
     }
 
