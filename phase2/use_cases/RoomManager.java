3860403238c64d592acc03a9d977bf0dbf610ae6
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2020-12-04 20:58:17 -0500

    update for getAvailableRoom

--- phase2/use_cases/RoomManager.java
+++ phase2/use_cases/RoomManager.java
@@ -175,7 +175,8 @@ public class RoomManager implements Serializable {
                 Event event = eventManager.getEventFromID(eventID);
 
                 // if the time conflicts, then the room is not available
-                if (event.getStartTime().equals(time)) {
+                if (!(Integer.parseInt(event.getStartTime())<= Integer.parseInt(time)) &&
+                        (Integer.parseInt(time) <= Integer.parseInt(event.getStartTime() +Integer.parseInt(duration)))) {
                     roomList.remove(changeIdTONum(roomID));
                 }
             }
