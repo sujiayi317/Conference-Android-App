57cb2c4adbac432496b39aee469c2e5165be08bb
NancyLi0637 <nancyl001237@gmail.com>
2020-12-13 15:37:18 +0800

    minor bug fix - missing import

--- phase2/207_demo/app/src/main/java/com/example/a207_demo/accountSystem/AccountActivity.java
+++ phase2/207_demo/app/src/main/java/com/example/a207_demo/accountSystem/AccountActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.a207_demo.R;
 import com.example.a207_demo.utility.ActivityCollector;
